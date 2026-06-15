2b207373e44eb5ebb23d6afefe63cf10938c25a3
Dhruv Jungi <dhruvjungi23@gmail.com>
2026-06-15 16:05:07 +0530

    feat: implement Dockerfile for multi-stage build and add .dockerignore

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,10 @@
 import express from "express";
 import cors from "cors";
 
-import dotenv from "dotenv/config"; 
+import dotenv from "dotenv/config";
+
+import fs from  "fs";
+import path from "path";
 
 import {clerkMiddleware} from "@clerk/express";
 
@@ -10,8 +13,11 @@ import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
+
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FORNTEND_URL;
+
+const publicDir = path.join(process.cwd(), "public");
 
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true})); //allowing frontend to access backend with credentials like cookies
@@ -20,6 +26,16 @@ app.use(clerkMiddleware());
 app.get("/health", (req, res) => {
     res.status(200).json({ok: true});
 });
+// if the public directory exists, serve the static files
+// this is for the production build
+
+
+if(fs.existsSync(publicDir)){
+    app.use(express.static(publicDir));
+    app.get("/{*any}",(req,res,next) => {
+        res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
