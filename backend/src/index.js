d119b484e9b0bbe6d3741e9e8de49b843f6c6206
Dhruv Jungi <dhruvjungi23@gmail.com>
2026-06-15 23:03:00 +0530

    fix: update import path for cron job module in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ import path from "path";
 
 import {clerkMiddleware} from "@clerk/express";
 
-import job from "./cron.js";
+import job from "./lib/cron.js";
 
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
