8b15384b576f7e06580e403e0ae4808338390562
Dhruv Jungi <dhruvjungi23@gmail.com>
2026-06-22 15:23:48 +0530

    feat: enhance frontend with authentication and theme features
    - Added axios for API requests and lucide-react for icons.
    - Integrated react-hot-toast for notifications.
    - Implemented authentication checks using @clerk/clerk-react and Zustand for state management.
    - Created new components for theme and wallpaper selection (ThemePresetPicker, ThemeToggle, WallpaperPicker).
    - Refactored AuthPage and ChatPage to utilize new components and improve layout.
    - Added loading state with PageLoader during authentication checks.
    - Established a socket connection for real-time user updates.
    - Created utility functions for message time formatting.
    - Enhanced UI with new Auth components (AuthHeader, AuthHeroPanel, AuthActionPanel, AuthCardShell).

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,38 @@ import { ThemeProvider } from "./context/ThemeContext";
 import {Route, Routes } from "react-router";
 import ChatPage from "./pages/ChatPage";
 import AuthPage from "./pages/AuthPage";
+import { useAuth } from "@clerk/clerk-react";
+import PageLoader from "./components/auth/PageLoader";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+
+import { Toaster } from "react-hot-toast";
+
 function App() {
 
   const { isSignedIn, isLoaded } = useAuth();
+  
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
 
-  //todo: make this a better component 
-  if (!isLoaded) return <p> loading...</p>;
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    if (isSignedIn) checkAuth();
+    else clearAuth();
+  }, [checkAuth, clearAuth, isLoaded, isSignedIn]);
+   
+  if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader/>;
   
   return (
     <ThemeProvider>
       <WallpaperProvider>
         <Routes>
           <Route path="/" element={isSignedIn ? <ChatPage/> :  <Navigate to={"/auth"} replace/>}/>
-          <Route path="/auth" element={!isSignedIn ? <AuthPage/> : <Navigate to={"/chat"} repalce/>}/>
+          <Route path="/auth" element={!isSignedIn ? <AuthPage/> : <Navigate to={"/"} repalce/>}/>
         </Routes>
+        <Toaster/>
       </WallpaperProvider>
     </ThemeProvider>
   )
